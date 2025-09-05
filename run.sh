bda542bce49e16a45fabc8d2858dfb256e3c01e2
dmm <107283522+DarthMooMancer@users.noreply.github.com>
2025-09-05 00:38:15 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mkdir build && cd build && cmake -S .. -B . && cmake --build . && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. && ln -sf compile_commands.json ../compile_commands.json
+mkdir build && cd build && cmake -S .. -B . && cmake --build . && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
